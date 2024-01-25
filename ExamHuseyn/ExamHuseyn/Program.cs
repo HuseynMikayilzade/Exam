735a544323028014940212befb57d36b49eb7f56
EminAbdulla <tu7exjg3e@code.edu.az>
2024-01-25 10:28:38 +0400

    added crud

--- ExamHuseyn/ExamHuseyn/Program.cs
+++ ExamHuseyn/ExamHuseyn/Program.cs
@@ -19,7 +19,13 @@ app.UseAuthentication();
 app.UseRouting();
 
 app.UseAuthorization();
-
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
+    );
+});
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
